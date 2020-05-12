edec587c893052eb9f892c2cec32cfc03c27fc59
juanr <juanr@LAPTOP-GDG9GQPJ>
2020-05-11 20:50:46 -0500

    Laboratorio 3 - Pruebas unitarias con JUnit, llevado a cabo.

--- src/Control/ValidarLogin.java
+++ src/Control/ValidarLogin.java
@@ -5,17 +5,19 @@
  */
 package Control;
 
-import Entidad.Sistema;
+import DAO.UsuarioDAO;
+//import Entidad.Sistema;
 import Entidad.Usuario;
-import Frontera.FramePrincipal;
+//import Frontera.FramePrincipal;
 
 /**
  *
  * @author juanr
  */
 public class ValidarLogin {
-    private Sistema sistema = FramePrincipal.sistema;
-
+    //private Sistema sistema = FramePrincipal.sistema;
+    private UsuarioDAO dao = new UsuarioDAO();
+    
     public ValidarLogin() {
     }
     
@@ -26,11 +28,14 @@ public class ValidarLogin {
         if (!verificarLongitudPassword(usuario.getPassword())) {
             return ("Longitud contraseña incorrecta");
         }
-        for (Usuario u: sistema.getUsuarios()) {
+        /*for (Usuario u: sistema.getUsuarios()) {
             if (u.getNombre().equals(usuario.getNombre()) && 
                     u.getPassword().equals(usuario.getPassword())) {
                 return ("Bienvenido");
             }
+        }*/
+        if (dao.leer(usuario)!=null) {
+            return ("Bienvenido");
         }
         return ("Datos incorrectos");
     }
